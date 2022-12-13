b5bf978341836e10277be3678ca12da19783e041
seokminy89 <92428806+seokminy89@users.noreply.github.com>
2022-12-14 08:02:45 +0900

    20221214 커밋
    - empty 이미지 설정.
    - Fab 버튼 설정.
    - platform.os로 os에 따라 다른 버튼 효과 적용

--- components/AddToDo.js
+++ components/AddToDo.js
@@ -1,14 +1,70 @@
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+  Platform,
+  TouchableNativeFeedback,
+} from 'react-native';
 
 function AddToDo() {
-  return <View style={styles.block} />;
+  const [text, setText] = useState('');
+
+  return (
+    <View style={styles.block}>
+      <TextInput
+        placeholder="할 일을 입력하세요"
+        style={styles.input}
+        value={text}
+        onChangeText={setText}
+      />
+      {Platform.OS === 'ios' ? (
+        <TouchableOpacity activeOpacity={0.5}>
+          <View style={styles.buttonStyle}>
+            <Image
+              source={require('../../assets/icons/add_white/add_white.png')}
+            />
+          </View>
+        </TouchableOpacity>
+      ) : (
+        <TouchableNativeFeedback>
+          <View style={styles.buttonStyle}>
+            <Image
+              source={require('../../assets/icons/add_white/add_white.png')}
+            />
+          </View>
+        </TouchableNativeFeedback>
+      )}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   block: {
+    backgroundColor: 'white',
     height: 64,
-    backgroundColor: 'red',
+    paddingHorizontal: 16,
+    borderColor: '#bdbdbd',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  input: {
+    flex: 1,
+    fontSize: 16,
+    paddingVertical: 8,
+  },
+  buttonStyle: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 48,
+    height: 48,
+    backgroundColor: '#282828',
+    borderRadius: 24,
   },
 });
 
--- components/Empty.js
+++ components/Empty.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 
 function Empty() {
   return (
     <View style={styles.block}>
+      <Image
+        source={require('../../assets/images/black_moon.jpeg')}
+        // source={require('../../assets/images/coco.jpeg')}
+        //source={require('../../assets/images/young_and_happy.png')}
+        //source={require('../../assets/images/guam_beach.jpeg')}
+        style={styles.image}
+        resizeMode="cover"
+      />
       <Text style={styles.description}>야호! 할 일이 없습니다.</Text>
     </View>
   );
@@ -16,8 +24,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   description: {
+    marginTop: 10,
     fontSize: 24,
     color: '#9e9e9e',
+  },
+  image: {
+    width: 300,
+    height: 200,
+    borderRadius: 15,
+    opacity: 0.8,
   },
 });
 
